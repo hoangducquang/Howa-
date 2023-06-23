7436b019d876fae3b887d2eda7e7bfe609d1c17d
Trần Anh Quân <anhquantran.info@gmail.com>
2023-06-23 17:35:04 +0700

    Add send feedback form
    (Đang fix selected stars)

--- public/scripts/coursedetail.js
+++ public/scripts/coursedetail.js
@@ -1,8 +1,64 @@
-const courseIdComment = sessionStorage.getItem('ssIdCourse')
+document.addEventListener('DOMContentLoaded', function() {
+  var openModalBtn = document.getElementById('open-modal-btn');
+  var closeModalBtn = document.getElementsByClassName('close')[0];
+  var modal = document.getElementById('feedback-modal');
+
+  // Hiển thị modal khi nhấp vào nút "Gửi phản hồi"
+  openModalBtn.addEventListener('click', function() {
+    modal.style.display = 'block';
+  });
+
+  // Đóng modal khi nhấp vào nút "Đóng" hoặc bên ngoài modal
+  closeModalBtn.addEventListener('click', function() {
+    modal.style.display = 'none';
+  });
+  window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
+
+  // Xử lý sự kiện gửi phản hồi
+  var submitFeedbackBtn = document.getElementById('submit-feedback-btn');
+  submitFeedbackBtn.addEventListener('click', function() {
+    var feedbackText = document.getElementById('feedback-text').value;
+    // Thực hiện xử lý gửi phản hồi (có thể là AJAX request tới máy chủ)
+    console.log('Feedback:', feedbackText);
+    modal.style.display = 'none';
+  });
+});
+
+const allStar = document.querySelectorAll('.rating .star');
+const ratingValue = document.querySelector('.rating input');
+
+allStar.forEach((item, idx)=> {
+	item.addEventListener('click', function () {
+		let click = 0;
+		ratingValue.value = idx + 1;
+
+		allStar.forEach(i=> {
+			i.classList.replace('bxs-star', 'bx-star');
+			i.classList.remove('active');
+		})
+		for(let i=0; i<allStar.length; i++) {
+			if(i <= idx) {
+				allStar[i].classList.replace('bx-star', 'bxs-star');
+				allStar[i].classList.add('active');
+			} else {
+				allStar[i].style.setProperty('--i', click);
+				click++;
+			}
+		}
+	})
+})
+
+
+const courseIdComment = sessionStorage.getItem("ssIdCourse");
 console.log(courseIdComment);
+console.log('test js');
 
 function showComment(props) {
-  $('.Customer-feedback').append(`
+  $(".Customer-feedback").append(`
     <div class="feedback-card">
       <div class="customer-feedback-header">
         <div class="customer-profile">
@@ -17,38 +73,38 @@ function showComment(props) {
         <p class="w3-small">${props.comment}</p>
       </div>
     </div>
-  `)
+  `);
 }
 
-function getComment(){
+function getComment() {
   fetch(`/api/comment/${courseIdComment}`)
-  .then(response => response.json())
-  .then(data => {
-    const allComment = data.commentAll
-    allComment.map(comment => {
-      showComment(comment)
+    .then((response) => response.json())
+    .then((data) => {
+      const allComment = data.commentAll;
+      allComment.map((comment) => {
+        showComment(comment);
+      });
     })
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
-getComment()
-var ssIdUser = sessionStorage.getItem('ssIdUser');
+getComment();
+var ssIdUser = sessionStorage.getItem("ssIdUser");
 console.log(ssIdUser);
 
-window.onload = function() {
-    if (ssIdUser != null) {
-      document.getElementById("menuLogin").style.display = "none";
-      document.getElementById("menuAccount").style.display = "block";
-    }else{
-      document.getElementById("menuLogin").style.display = "block";
-      document.getElementById("menuAccount").style.display = "none";
-    }
+window.onload = function () {
+  if (ssIdUser != null) {
+    document.getElementById("menuLogin").style.display = "none";
+    document.getElementById("menuAccount").style.display = "block";
+  } else {
+    document.getElementById("menuLogin").style.display = "block";
+    document.getElementById("menuAccount").style.display = "none";
+  }
 };
 
-const categoryText = document.getElementById('category');
+const categoryText = document.getElementById("category");
 const text = categoryText.textContent;
 const capitalizedText = text.charAt(0).toUpperCase() + text.slice(1);
 categoryText.textContent = capitalizedText;
